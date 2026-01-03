8cdcf350802ea833ef3d40dda58e81ddb102840b
Carlos Ferreira <carlos.eduardo@synvia.com>
2026-01-03 15:44:33 -0300

    adicionando comentarios

--- arrays.js
+++ arrays.js
@@ -1,7 +1,7 @@
 const listaEstudantes = ['João', 'Juliana', 'Ana', 'Caio', 'Lara', 'Marjorie', 'Guilherme', 'Aline', 'Fabiana', 'André', 'Carlos', 'Paulo', 'Bia', 'Vivian', 'Isabela', 'Vinícius', 'Renan', 'Renata', 'Daisy', 'Camilo'];
 
 const sala1 = listaEstudantes.slice(0, listaEstudantes.length/2);
-const sala2 = listaEstudantes.slice(listaEstudantes.length/2);
+const sala2 = listaEstudantes.slice(listaEstudantes.length/2); //nome do array.slice == corta o array usando os parametors que voce definir
 
 console.log(sala1);
 console.log(sala2);
