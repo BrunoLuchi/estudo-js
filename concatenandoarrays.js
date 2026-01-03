68e8f35043eed4d44cb6f5baf55c0af77a023616
Carlos Ferreira <carlos.eduardo@synvia.com>
2026-01-03 16:00:18 -0300

    comentarios sobre concat

---
+++ concatenandoarrays.js
@@ -0,0 +1,5 @@
+const salaJS = ['Evaldo', 'Camis', 'Mari'];
+const salaPython = ['Ju', 'Leo', 'Raquel'];
+
+const salasUnificadas = salaJS.concat(salaPython);//nome do array.concat(outro array) == concatena arrays
+console.log(salasUnificadas);
