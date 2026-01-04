7fdd8e65ebe96056994dafc26e036b4edefb5559
Carlos Ferreira <carlos.eduardo@synvia.com>
2026-01-04 09:32:46 -0300

    nonvo arquivo sobre fitlrar elemntos em arrays

---
+++ filtrar.js
@@ -0,0 +1,8 @@
+const alunos = ['Ana', 'Marcos', 'Maria', 'Mauro'];
+const medias = [7, 4.5, 8, 7.5];
+
+const reprovados = alunos.filter((_, indice) => { //nome do array.filter(elemnto filtrado,indice) == para filtrar elemntos em arrays
+  return medias[indice] < 7;
+});
+
+console.log(reprovados);
